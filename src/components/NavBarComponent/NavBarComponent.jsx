7bee2b9a3ba5d4c37c0f4c1c069eb2b18030dc4f
Pipe Daza <pipedaza23@gmail.com>
2023-10-31 21:01:58 -0300

    PreEntrega1 Juan Felipe Daza

--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -23,16 +23,6 @@ const NavBarComponent = () => {
         >
           <Nav.Link href="#action1">Home</Nav.Link>
           <Nav.Link href="#action2">Products</Nav.Link>
-          {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action5">
-              Something else here
-            </NavDropdown.Item>
-          </NavDropdown> */}
           <Nav.Link href="#action3" >
             Nosotros
           </Nav.Link>
